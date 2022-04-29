554776e69d8b715db832097caa9156da024de63c
AnusreeKoottungal <anushreekoottungal@gmail.com>
2022-04-29 19:09:12 +0530

    carousal

--- src/layout/Carousal.js
+++ src/layout/Carousal.js
@@ -2,17 +2,23 @@ import Carousel from "react-bootstrap/Carousel";
 import food from "../resources/food.jpg";
 import book from "../resources/book.jpg";
 import travel from "../resources/travel.jpg";
+
 function Carousal() {
+  const title = {
+    fontFamily: "'Square Peg', cursive",
+    fontWeight: "bold",
+    fontSize: "xx-large",
+  };
   return (
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100" src={travel} alt="Travel" />
+        <img className="card-img-top" src={travel} alt="Travel"></img>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={food} alt="Food" />
+        <img className="card-img-top" src={food} alt="Food" />
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={book} alt="Books" />
+        <img className="card-img-top" src={book} alt="Books" />
       </Carousel.Item>
     </Carousel>
   );
